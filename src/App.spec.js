60c8c4730d3ab071407050477997ea863cb6b80b
Frederick Johnson <fritzj96@gmail.com>
2020-04-26 01:24:09 -0700

    removed: testing tools

--- src/App.spec.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import App from "./App";
-import { render } from "@testing-library/react";
-
-describe("App Component", () => {
-  test("SMOKE: renders without crashing", () => {
-    render(<App />);
-  });
-});
