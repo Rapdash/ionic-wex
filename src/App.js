ce1a1f1f93b55cf2a551e8ce7785c562acb2b78a
Frederick Johnson <fritzj96@gmail.com>
2020-04-29 11:39:06 -0700

    adding: routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
 import React, { useEffect } from "react";
-import { IonApp } from "@ionic/react";
-import { checkToken } from "./state/authSlice";
+import { Route, Redirect } from "react-router-dom";
+import { IonReactRouter } from "@ionic/react-router";
 import { useSelector, useDispatch } from "react-redux";
+import { IonApp, IonSplitPane, IonRouterOutlet } from "@ionic/react";
+
+import { checkToken } from "./state/authSlice";
+import { Menu } from "./components/Menu";
 
 export const App = () => {
   const { loading, user, loggedIn } = useSelector((state) => state.auth);
   const dispatch = useDispatch();
-  console.log(loggedIn);
+
   useEffect(() => {
     dispatch(checkToken());
   }, [dispatch]);
-  return <IonApp />;
+
+  return (
+    <IonApp>
+      <IonReactRouter>
+        <IonSplitPane contentId="main">
+          <Menu menuEnabled={loggedIn} />
+          <IonRouterOutlet id="main">
+            <Route path="/listings" component={() => <div>Listings</div>} />
+            <Route path="/login" component={() => <div>login</div>} />
+            <Redirect exact from="/" to="/listings" />
+            {!loading && !loggedIn && <Redirect to="/login" />}
+          </IonRouterOutlet>
+        </IonSplitPane>
+      </IonReactRouter>
+    </IonApp>
+  );
 };
---
+++ src/components/Menu.js
@@ -0,0 +1,54 @@
+import React from "react";
+import {
+  IonMenu,
+  IonContent,
+  IonList,
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+  IonItem,
+  IonLabel,
+  IonFooter,
+  IonMenuToggle,
+} from "@ionic/react";
+import { useHistory } from "react-router-dom";
+
+export const Menu = ({ menuEnabled }) => {
+  const history = useHistory();
+
+  return (
+    <IonMenu type="overlay" disabled={!menuEnabled} contentId="main">
+      <IonHeader>
+        <IonToolbar color="primary">
+          <IonTitle>Menu</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent forceOverscroll={false}>
+        <IonMenuToggle autoHide={false}>
+        <IonList style={{ paddingTop: 0 }}>
+          <IonItem button onClick={() => history.push("/all-listings")}>
+            <IonLabel>All Listings</IonLabel>
+          </IonItem>
+          <IonItem button onClick={() => history.push("/my-listings")}>
+            <IonLabel>My Listings</IonLabel>
+          </IonItem>
+          <IonItem button onClick={() => history.push("/incoming-offers")}>
+            <IonLabel>Incoming Offers</IonLabel>
+          </IonItem>
+          <IonItem button onClick={() => history.push("/incoming-offers")}>
+            <IonLabel>Outgoing Offers</IonLabel>
+          </IonItem>
+          <IonItem button onClick={() => history.push("/account")}>
+            <IonLabel>Account Management</IonLabel>
+          </IonItem>
+        </IonList>
+        </IonMenuToggle>
+      </IonContent>
+      <IonFooter>
+            <IonItem lines={false}>
+              <IonLabel>© 2020 The Water Agency, Inc.</IonLabel>
+            </IonItem>
+      </IonFooter>
+    </IonMenu>
+  );
+};
