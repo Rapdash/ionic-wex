3138cf00bc28a438429ca3721dc3dbd12ddfc08f
Frederick Johnson <fritzj96@gmail.com>
2020-04-29 12:31:59 -0700

    added: listingSlice

--- src/pages/AllListingsPage.js
+++ src/pages/AllListingsPage.js
@@ -1,11 +1,20 @@
 import React from "react";
-import { IonPage } from "@ionic/react";
+import { IonPage, IonContent, IonList, IonItem } from "@ionic/react";
+
 import { PageHeader } from "../components/PageHeader";
+import { useSelector } from "react-redux";
 
 export const AllListingsPage = () => {
+  const {} = useSelector((state) => state.listing);
+  const { user } = useSelector((state) => state.auth);
   return (
     <IonPage>
       <PageHeader title="All Listings" />
+      <IonContent>
+        <IonList>
+          <IonItem></IonItem>
+        </IonList>
+      </IonContent>
     </IonPage>
   );
 };
---
+++ src/state/listingSlice.js
@@ -0,0 +1,40 @@
+import { createSlice } from "@reduxjs/toolkit";
+import Axios from "axios";
+
+export const listingSlice = createSlice({
+  name: "listing",
+  initialState: { listings: [], loading: true, error: null },
+  reducers: {
+    loadListingsSuccess: (_state, action) => {
+      return {
+        listings: action.payload,
+        loading: false,
+        error: null,
+      };
+    },
+    loadListingsFailure: (_state, action) => ({
+      listings: [],
+      loading: false,
+      error: action.payload,
+    }),
+  },
+});
+
+export const {
+  loadListingsSuccess,
+  loadListingsFailure,
+} = listingSlice.actions;
+
+export const authReducer = listingSlice.reducer;
+
+export const loadAllListings = () => async (dispatch) => {
+  try {
+    const res = await Axios.get(`${process.env.REACT_APP_API_URL}/listing`, {
+      headers: { Authorization: localStorage.getItem("auth_token") },
+    });
+    console.log(res.data);
+    dispatch(loadListingsSuccess(res.data));
+  } catch {
+    dispatch(loadListingsFailure());
+  }
+};
